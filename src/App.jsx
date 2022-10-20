c36c0d5ad4a6d6ac09f8c50be8da52daa99d543e
Kirill Smirnov <clean.energy.flow@gmail.com>
2022-10-20 19:30:58 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './scss/assets/reset.scss'
+import './scss/assets/fonts.scss'
+import './scss/App.scss'
+import { useEffect } from 'react';
+
+function App() {
+
+  setTimeout(() => {
+    let loader = document.querySelector('.content-load')
+    loader.style.transform = "translateX(100vw)"
+  }, 100)
+  
+  return (
+    <div className="App">
+      <div className="content">
+        <div className="content-load"></div>
+        <div className="content-main">
+          <div className="main-title-block">
+            <div className="main-title">Регенерация вместо покупки</div>
+            <div className="main-subtitle">Восстановление свойств масел <br/> для повторного использования</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
