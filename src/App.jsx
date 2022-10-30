aabaa3b784a69441cd53d2a0339f5e596771c49c
Kirill Smirnov <clean.energy.flow@gmail.com>
2022-10-30 19:03:38 +0300

    30 окт 2022 г. 19:03:38

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import './scss/assets/reset.scss'
 import './scss/assets/fonts.scss'
 import './scss/App.scss'
 import { ReactComponent as Wave } from './media/img/wave.svg';
+import { ReactComponent as Mask } from './media/img/mask.svg';
 import { Content } from './content'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import $ from 'jquery'
 
 function App() {
@@ -23,6 +24,8 @@ function App() {
     let lineLeft = document.querySelector('.line-left')
     let lineRight = document.querySelector('.line-right')
     let flipAnimationBlock = document.querySelectorAll('.flip-animation-block')
+    let kamazAnimation = document.querySelector('.kamaz-animation')
+    let kamazAnimationBlock = document.querySelector('.kamaz-animation-block')
 
     let flipAnimationLineOne = flipAnimationBlock[0]
     let flipAnimationLineTwo = flipAnimationBlock[1]
@@ -36,7 +39,6 @@ function App() {
 
 
     let clientHeight = document.documentElement.clientHeight
-    let flipTrigger = false
     let scrollCount = 0
 
     let firstFLip = false
@@ -73,20 +75,19 @@ function App() {
       })
 
 
-      if(flipAnimationLineOne.getBoundingClientRect().top <= clientHeight * 0.9){
+      if (flipAnimationLineOne.getBoundingClientRect().top <= clientHeight * 0.9) {
 
-        if(firstFLip === false){
-          OneOne.style.opacity = 1;  OneOne.style.transform = 'translateY(0)'
+        if (firstFLip === false) {
+          OneOne.style.opacity = 1; OneOne.style.transform = 'translateY(0)'
           setTimeout(() => {
             OneTwo.style.opacity = 1; OneTwo.style.transform = 'translateY(0)'
           }, 100);
           scrollCount = $(window).scrollTop()
           firstFLip = true
-          console.log('firstFlipOn')
         }
 
-        if($(window).scrollTop() - scrollCount >= 1000){ //Прикрутить адаптив
-          if(secondFLip === false){
+        if ($(window).scrollTop() - scrollCount >= 1000) { //Прикрутить адаптив
+          if (secondFLip === false) {
             OneOne.style.opacity = 0; OneOne.style.transform = 'translateY(-116px)'; //Прикрутить адаптив
             TwoOne.style.opacity = 1; TwoOne.style.transform = 'translateY(-116px)';  //Прикрутить адаптив
             setTimeout(() => {
@@ -96,7 +97,7 @@ function App() {
             secondFLip = true
           }
         } else {
-          if(secondFLip === true){
+          if (secondFLip === true) {
             TwoOne.style.opacity = 0; TwoOne.style.transform = 'translateY(0)';
             OneOne.style.opacity = 1; OneOne.style.transform = 'translateY(0)';
             setTimeout(() => {
@@ -107,21 +108,29 @@ function App() {
           }
         }
       } else {
-        if(firstFLip === true){
+        if (firstFLip === true) {
           OneOne.style.opacity = 0; OneOne.style.transform = 'translateY(116px)' //Прикрутить адаптив
           setTimeout(() => {
             OneTwo.style.opacity = 0; OneTwo.style.transform = 'translateY(116px)' //Прикрутить адаптив
           }, 100);
           firstFLip = false
-          console.log('firstFlipOut')
         }
+      }
+
+      if ((kamazAnimation.getBoundingClientRect().top <= clientHeight * 0.7)) {
+        kamazAnimationBlock.style.transform='translateX(1100px)'
+      } else {
+        kamazAnimationBlock.style.transform='translateX(0)'
       }
 
     })
   }, [])
 
+  const [answer, setAnswer] = useState([`.01 ${Content.QA.questions[0].title}`, Content.QA.questions[0].text])
 
-
+  const showAnswer = (index) => {
+    setAnswer([`.0${index+1} ${Content.QA.questions[index].title}`, Content.QA.questions[index].text])
+  }
 
   return (
     <div className="App">
@@ -154,40 +163,40 @@ function App() {
               <div className="line-right"></div>
             </div>
             <div className="tezis-wrap opacity-animation-block">
-            {Content.aboutRegeneration.tezisGroup.map(tezis => {
-              return (
-                <div className="tezis">
-                  <div className="tezis-name">{tezis.name}</div>
-                  <div className="tezis-points"></div>
-                  <div className="tezis-text">{tezis.text}</div>
-                </div>
-              )
-            })}
-             </div>
+              {Content.aboutRegeneration.tezisGroup.map(tezis => {
+                return (
+                  <div className="tezis">
+                    <div className="tezis-name">{tezis.name}</div>
+                    <div className="tezis-points"></div>
+                    <div className="tezis-text">{tezis.text}</div>
+                  </div>
+                )
+              })}
+            </div>
           </div>
         </div>
       </div>
 
       <div className="block-More">
-          <div className="more-wrap">
-            <div className="more-info">
-              <div className="more-title title-animation">
-                <div className="title-animation-block"></div>
-                {Content.aboutMore.title}
-              </div>
-              <div className="more-text text-animation">
-                <div className="text-animation-block"></div>
-                {Content.aboutMore.text}
-              </div>
+        <div className="more-wrap">
+          <div className="more-info">
+            <div className="more-title title-animation">
+              <div className="title-animation-block"></div>
+              {Content.aboutMore.title}
             </div>
-            <div className="more-content">
-               <div className="more-lottie"></div>
-               <div className="more-advantage-group">
-               {Content.aboutMore.advantageGroup.map(advatnageLine => {
+            <div className="more-text text-animation">
+              <div className="text-animation-block"></div>
+              {Content.aboutMore.text}
+            </div>
+          </div>
+          <div className="more-content">
+            <div className="more-lottie"></div>
+            <div className="more-advantage-group">
+              {Content.aboutMore.advantageGroup.map(advatnageLine => {
                 return (
                   <div className="advantage-line flip-animation-block">
-                    {advatnageLine.map(advantage=>{
-                      return(
+                    {advatnageLine.map(advantage => {
+                      return (
                         <div className="advantage-example">
                           <div className="advantage-title">{advantage.title}</div>
                           <div className="advantage-text">{advantage.text}</div>
@@ -196,20 +205,83 @@ function App() {
                     })}
                   </div>
                 )
-               })}
-                  {/* <div className="advantage-line">
-                      <div className="advantage-example">
-                        <div className="advantage-title"></div>
-                        <div className="advantage-text"></div>
-                      </div>
-                  </div> */}
-               </div>
+              })}
             </div>
-           
           </div>
+        </div>
       </div>
 
-      <div className="next-block"></div>
+      <div className="block-Work">
+        <div className="work-wrap">
+          <div className="work-title title-animation">
+            <div className="title-animation-block"></div>
+            {Content.aboutWork.title}
+          </div>
+          <div className="work-info">
+            <div className="work-kamaz kamaz-animation">
+              <div className="kamaz-mask kamaz-animation-block"><Mask/></div>
+              <img src={require('./media/img/murm.png')} alt="" />
+            </div>
+            <div className="work-points"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="block-Form">
+        <div className="Form-title">{Content.Form.title}</div>
+        <div className="Form-button">{Content.Form.button}</div>
+        <div className="Form-body"></div>
+      </div>
+
+      <div className="block-Partners">
+        <div className="Partners-wrap">
+          <div className="partners-title title-animation">
+            <div className="title-animation-block"></div>
+            {Content.aboutPartners.title}
+          </div>
+          <div className="partners-row">
+            {Content.aboutPartners.partners.map((partner,index)=>{
+              return(
+                // <a href='' about='blanc'></a>
+                <div className="partner">
+                  <div className="partner-img opacity-animation-block"><img src={require(`./media/img/partners/${index+1}.png`)} alt="" /></div>
+                  <div className="partner-name text-animation">
+                    <div className="text-animation-block"></div>
+                    {partner.name}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="next-block"></div> */}
+
+      <div className="block-QA">
+        <div className="QA-wrap">
+           <div className="QA-title title-animation">
+              <div className="title-animation-block"></div>
+              {Content.QA.title}
+            </div>
+           <div className="QA">
+            <div className="QA-questions">
+              {Content.QA.questions.map((question, index)=>{
+                return(
+                  <div className="QA-question" onClick={()=>{showAnswer(index)}}>
+                    <div className="QA-question-number">.0{index+1}</div>
+                    <div className="QA-question-title">{question.title}</div>
+                  </div>
+                )
+              })}
+            </div>
+            <div className="QA-answer-panel">
+                <div className="question">{answer[0]}</div>
+                <div className="answer">{answer[1]}</div>
+            </div>
+           </div>
+        </div>
+      </div>
 
       <div className="loader">
         <div className="loader-panel"></div>
