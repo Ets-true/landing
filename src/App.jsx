6be7f33a87bc152be412bcb17d3c316847c0b99f
Kirill Smirnov <clean.energy.flow@gmail.com>
2022-11-07 17:51:06 +0300

    7 ноя 2022 г. 17:51:06

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './scss/assets/fonts.scss'
 import './scss/App.scss'
 import { ReactComponent as Wave } from './media/img/wave.svg';
 import { ReactComponent as Mask } from './media/img/mask.svg';
-import { Content } from './content'
+import { Content } from './Content'
 import { useEffect, useState } from 'react';
 import $ from 'jquery'
 
@@ -43,6 +43,11 @@ function App() {
 
     let firstFLip = false
     let secondFLip = false;
+
+    const Footer = document.querySelector('.block-Footer')
+    const Body = document.querySelector('.App-body')
+    let res
+    let footerTrigger = false
 
     window.addEventListener('scroll', function (e) {
       // let windowTop = $(window).scrollTop() + document.documentElement.clientHeight + 500
@@ -118,33 +123,47 @@ function App() {
       }
 
       if ((kamazAnimation.getBoundingClientRect().top <= clientHeight * 0.7)) {
-        kamazAnimationBlock.style.transform='translateX(1100px)'
+        kamazAnimationBlock.style.transform = 'translateX(1100px)'
       } else {
-        kamazAnimationBlock.style.transform='translateX(0)'
+        kamazAnimationBlock.style.transform = 'translateX(0)'
       }
+
+      // if ((Footer.getBoundingClientRect().top <= clientHeight)) {
+        res =  (Footer.getBoundingClientRect().top / clientHeight) * 1.97
+        console.log(res)
+        if (res <= 1 && footerTrigger == false){
+          footerTrigger = true
+          res = 0.95
+          Body.style.transform = `scale(${res})`
+        }
+        if(res > 1 && footerTrigger) {
+          footerTrigger = false
+          Body.style.transform = `scale(1)`
+        }
+      // }
 
     })
 
 
     let questions = document.querySelectorAll(".QA-question");
-    
+
     let pointPositions = []
     let startPosition = 53
     let endPosition = questions[6].offsetTop + questions[6].offsetHeight
 
     questions.forEach(question => {
-      let position = Math.floor((question.offsetTop + question.offsetHeight/2 - startPosition) / endPosition * 1000)
+      let position = Math.floor((question.offsetTop + question.offsetHeight / 2 - startPosition) / endPosition * 1000)
       console.log(position)
       pointPositions.push(position)
     })
     console.log(pointPositions)
 
-    const getNumber = (arr, searchNum) => 
+    const getNumber = (arr, searchNum) =>
       arr.find(it => Math.abs(it - searchNum) === Math.min(...arr.map(it => Math.abs(it - searchNum))));
 
     let control = document.querySelector(".control");
-    
-    control.oninput = function() {
+
+    control.oninput = function () {
       console.log(getNumber(pointPositions, this.value))
     }
   }, [])
@@ -152,169 +171,199 @@ function App() {
   const [answer, setAnswer] = useState([`.01 ${Content.QA.questions[0].title}`, Content.QA.questions[0].text])
 
   const showAnswer = (index) => {
-    setAnswer([`.0${index+1} ${Content.QA.questions[index].title}`, Content.QA.questions[index].text])
+    setAnswer([`.0${index + 1} ${Content.QA.questions[index].title}`, Content.QA.questions[index].text])
   }
+
+
 
 
   return (
     <div className="App">
-      <div className="block-Regeneration">
-        <div className="logo"><img src={require('./media/img/RM-logo.png')} alt="" /></div>
-        <div className="title-block">
-          <div className="title">{Content.Regeneration.title}</div>
-          <div className="subtitle">{Content.Regeneration.subtitle}</div>
+      <div className="App-body">
+        <div className="block-Regeneration">
+          <div className="logo"><img src={require('./media/img/RM-logo.png')} alt="" /></div>
+          <div className="title-block">
+            <div className="title">{Content.Regeneration.title}</div>
+            <div className="subtitle">{Content.Regeneration.subtitle}</div>
+          </div>
+          <div className="wave-block"><Wave /></div>
         </div>
-        <div className="wave-block"><Wave /></div>
-      </div>
-      <div className="background-blue"></div>
+        <div className="background-blue"></div>
 
-      <div className="block-About">
-        <div className="about-wrap">
-          <div className="about-info">
-            <div className="about-title title-animation">
-              <div className="title-animation-block"></div>
-              Что такое <span>регенерация</span>
+        <div className="block-About">
+          <div className="about-wrap">
+            <div className="about-info">
+              <div className="about-title title-animation">
+                <div className="title-animation-block"></div>
+                Что такое <span>регенерация</span>
+              </div>
+              <div className="about-text text-animation">
+                <div className="text-animation-block"></div>
+                {Content.aboutRegeneration.text}
+              </div>
             </div>
-            <div className="about-text text-animation">
-              <div className="text-animation-block"></div>
-              {Content.aboutRegeneration.text}
+            <div className="about-tezis-group">
+              <div className="line">
+                <div className="line-left"></div>
+                <div className="line-title opacity-animation-block">{Content.aboutRegeneration.lineTitle}</div>
+                <div className="line-right"></div>
+              </div>
+              <div className="tezis-wrap opacity-animation-block">
+                {Content.aboutRegeneration.tezisGroup.map(tezis => {
+                  return (
+                    <div className="tezis">
+                      <div className="tezis-name">{tezis.name}</div>
+                      <div className="tezis-points"></div>
+                      <div className="tezis-text">{tezis.text}</div>
+                    </div>
+                  )
+                })}
+              </div>
             </div>
           </div>
-          <div className="about-tezis-group">
-            <div className="line">
-              <div className="line-left"></div>
-              <div className="line-title opacity-animation-block">{Content.aboutRegeneration.lineTitle}</div>
-              <div className="line-right"></div>
+        </div>
+
+        <div className="block-More">
+          <div className="more-wrap">
+            <div className="more-info">
+              <div className="more-title title-animation">
+                <div className="title-animation-block"></div>
+                {Content.aboutMore.title}
+              </div>
+              <div className="more-text text-animation">
+                <div className="text-animation-block"></div>
+                {Content.aboutMore.text}
+              </div>
             </div>
-            <div className="tezis-wrap opacity-animation-block">
-              {Content.aboutRegeneration.tezisGroup.map(tezis => {
+            <div className="more-content">
+              <div className="more-lottie"></div>
+              <div className="more-advantage-group">
+                {Content.aboutMore.advantageGroup.map(advatnageLine => {
+                  return (
+                    <div className="advantage-line flip-animation-block">
+                      {advatnageLine.map(advantage => {
+                        return (
+                          <div className="advantage-example">
+                            <div className="advantage-title">{advantage.title}</div>
+                            <div className="advantage-text">{advantage.text}</div>
+                          </div>
+                        )
+                      })}
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="block-Work">
+          <div className="work-wrap">
+            <div className="work-title title-animation">
+              <div className="title-animation-block"></div>
+              {Content.aboutWork.title}
+            </div>
+            <div className="work-info">
+              <div className="work-kamaz kamaz-animation">
+                <div className="kamaz-mask kamaz-animation-block"><Mask /></div>
+                <img src={require('./media/img/murm.png')} alt="" />
+              </div>
+              <div className="work-points">
+                {Content.aboutWork.pointNames.map((pointName, index) => {
+                  return (
+                    <div className="point-example">
+                      <div className="point-num">.0{index + 1}</div>
+                      <div className="point-name">{pointName}</div>
+                    </div>
+                  )
+                })}
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="block-Form">
+          <div className="Form-title">{Content.Form.title}</div>
+          <div className="Form-button">{Content.Form.button}</div>
+          <div className="Form-body"></div>
+        </div>
+
+        <div className="block-Partners">
+          <div className="Partners-wrap">
+            <div className="partners-title title-animation">
+              <div className="title-animation-block"></div>
+              {Content.aboutPartners.title}
+            </div>
+            <div className="partners-row">
+              {Content.aboutPartners.partners.map((partner, index) => {
                 return (
-                  <div className="tezis">
-                    <div className="tezis-name">{tezis.name}</div>
-                    <div className="tezis-points"></div>
-                    <div className="tezis-text">{tezis.text}</div>
+                  // <a href='' about='blanc'></a>
+                  <div className="partner">
+                    <div className="partner-img opacity-animation-block"><img src={require(`./media/img/partners/${index + 1}.png`)} alt="" /></div>
+                    <div className="partner-name text-animation">
+                      <div className="text-animation-block"></div>
+                      {partner.name}
+                    </div>
                   </div>
                 )
               })}
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="block-More">
-        <div className="more-wrap">
-          <div className="more-info">
-            <div className="more-title title-animation">
-              <div className="title-animation-block"></div>
-              {Content.aboutMore.title}
-            </div>
-            <div className="more-text text-animation">
-              <div className="text-animation-block"></div>
-              {Content.aboutMore.text}
-            </div>
-          </div>
-          <div className="more-content">
-            <div className="more-lottie"></div>
-            <div className="more-advantage-group">
-              {Content.aboutMore.advantageGroup.map(advatnageLine => {
-                return (
-                  <div className="advantage-line flip-animation-block">
-                    {advatnageLine.map(advantage => {
-                      return (
-                        <div className="advantage-example">
-                          <div className="advantage-title">{advantage.title}</div>
-                          <div className="advantage-text">{advantage.text}</div>
-                        </div>
-                      )
-                    })}
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="block-Work">
-        <div className="work-wrap">
-          <div className="work-title title-animation">
-            <div className="title-animation-block"></div>
-            {Content.aboutWork.title}
-          </div>
-          <div className="work-info">
-            <div className="work-kamaz kamaz-animation">
-              <div className="kamaz-mask kamaz-animation-block"><Mask/></div>
-              <img src={require('./media/img/murm.png')} alt="" />
-            </div>
-            <div className="work-points"></div>
-          </div>
-        </div>
-      </div>
-
-      <div className="block-Form">
-        <div className="Form-title">{Content.Form.title}</div>
-        <div className="Form-button">{Content.Form.button}</div>
-        <div className="Form-body"></div>
-      </div>
-
-      <div className="block-Partners">
-        <div className="Partners-wrap">
-          <div className="partners-title title-animation">
-            <div className="title-animation-block"></div>
-            {Content.aboutPartners.title}
-          </div>
-          <div className="partners-row">
-            {Content.aboutPartners.partners.map((partner,index)=>{
-              return(
-                // <a href='' about='blanc'></a>
-                <div className="partner">
-                  <div className="partner-img opacity-animation-block"><img src={require(`./media/img/partners/${index+1}.png`)} alt="" /></div>
-                  <div className="partner-name text-animation">
-                    <div className="text-animation-block"></div>
-                    {partner.name}
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      </div>
-
-      {/* <div className="next-block"></div> */}
-
-      <div className="block-QA">
-        <div className="QA-wrap">
-           <div className="QA-title title-animation">
+        <div className="block-QA">
+          <div className="QA-wrap">
+            <div className="QA-title title-animation">
               <div className="title-animation-block"></div>
               {Content.QA.title}
             </div>
-           <div className="QA">
-            <div className="QA-questions">
-              <div class="control-wrap">
-                <div className="control-body">
-                  <div className="line"></div>
-                   <input type="range" min={1} max={1000} defaultValue="25" className="control"/>
-                </div>
-              </div>
-              {Content.QA.questions.map((question, index)=>{
-                return(
-                  <div className="QA-question" onClick={()=>{showAnswer(index)}}>
-                    <div className="QA-question-number">.0{index+1}</div>
-                    <div className="QA-question-title">{question.title}</div>
+            <div className="QA">
+              <div className="QA-questions">
+                <div class="control-wrap">
+                  <div className="control-body">
+                    <div className="line"></div>
+                    <input type="range" min={1} max={1000} defaultValue="25" className="control" />
                   </div>
-                )
-              })}
-            </div>
-            <div className="QA-answer-panel">
+                </div>
+                {Content.QA.questions.map((question, index) => {
+                  return (
+                    <div className="QA-question" onClick={() => { showAnswer(index) }}>
+                      <div className="QA-question-number">.0{index + 1}</div>
+                      <div className="QA-question-title">{question.title}</div>
+                    </div>
+                  )
+                })}
+              </div>
+              <div className="QA-answer-panel">
                 <div className="question">{answer[0]}</div>
                 <div className="answer">{answer[1]}</div>
+              </div>
             </div>
-           </div>
+          </div>
+        </div>
+
+        <div className="loader">
+          <div className="loader-panel"></div>
         </div>
       </div>
 
-      <div className="loader">
-        <div className="loader-panel"></div>
+      <div className="block-Footer">
+        <div className="Footer-row">
+          <div className="Footer-form">
+            <div className="form-title">{Content.Footer.form.formText}</div>
+            <div className="form-button">{Content.Footer.form.formButton}</div>
+            <div className="RM">{Content.Footer.form.RM}</div>
+            <div className="copyright">{Content.Footer.form.copyright}</div>
+          </div>
+          <div className="Footer-info">
+            <div className="info-block">
+              <div className="title"></div>
+              <div className="text"></div>
+            </div>
+          </div>
+        </div>
+        <div className="Footer-department"></div>
       </div>
     </div>
   );
